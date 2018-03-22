c4af60de834f57df2e8951a0193f0fe4124dd6f4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-22 04:45:11 -0400

    Update TimeDateZone.jl

--- src/types/TimeDateZone.jl
+++ src/types/TimeDateZone.jl
@@ -1,1 +1,19 @@
+struct TimeDateZone{T} <: AbstractTime
+    timedate::TimeDate
+    in_zone::T                # VariableTimeZone or FixedTimeZone
+    at_zone::FixedTimeZone
+  
+    function TimeDateZone{T}(timedate::TimeDate, in_zone::T, at_zone::FixedTimeZone) where {T<:FixedTimeZone}
+        new{T}(timedate, in_zone, at_zone)
+    end
+  
+    function TimeDateZone{T}(timedate::TimeDate, in_zone::T, at_zone::FixedTimeZone) where {T<:VariableTimeZone}
+        new{T}(timedate, in_zone, at_zone)
+    end
+end
 
+TimeDateZone{T}(timedate::TimeDate, in_zone::T) where {T<:VariableTimeZone} =
+    TimeDateZone{VariableTimeZone}(timedate, in_zone)
+
+TimeDateZone{T}(timedate::TimeDate, in_zone::T) where {T<:FixedTimeZone} =
+    TimeDateZone{FixedTimeZone}(timedate, in_zone)
