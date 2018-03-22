ba7c0c3d84a490b66a72bfc66f4c6ce25ed3f3e1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-22 04:11:35 -0400

    Update NanoTimes.jl

--- src/NanoTimes.jl
+++ src/NanoTimes.jl
@@ -1,11 +1,25 @@
 module NanoTimes
 
-export TimeDate, TimeDateZone
+export TimeDate, TimeDateZone,
+       Period, CompoundPeriod, ReverseCompoundPeriod,
+       TimeZone, @tz_str, ZonedDateTime,
+       Time, Date, DateTime,
+       Year, Month, Day, Hour, Minute, Second,
+       Millisecond, Microsecond, Nanosecond,
+       Years, Months, Days, Hours, Minutes, Seconds,
+       Milliseconds, Microseconds, Nanoseconds
 
-import Dates: AbstractTime, TimeType
+
+import Dates: AbstractTime, TimeType,
+              Time, Date, DateTime,
+              Year, Month, Day, Hour, Minute, Second,
+              Millisecond, Microsecond, Nanosecond,
+              year, month, day, hour, minute, second,
+              millisecond, microsecond, nanosecond
+
 using Dates
 
-import TimeZones: TimeZone, ZonedDateTime, UTCOffset
+import TimeZones: TimeZone, @tz_str, ZonedDateTime, UTCOffset
 using TimeZones
 
 using CompoundPeriods
