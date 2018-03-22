a03932bf286dfa9c471290f152ab5d9ffecb4818
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-22 03:51:08 -0400

    Create NanoTimes.jl

---
+++ src/NanoTimes.jl
@@ -0,0 +1,14 @@
+module NanoTimes
+
+export TimeDate, TimeDateZone
+
+import Dates: AbstractTime, TimeTypes, Period, CompoundPeriod
+using Dates
+
+import TimeZones: TimeZone, ZonedDateTime, UTCOffset
+using TimeZones
+
+using CompoundPeriod
+
+
+end # NanoTimes
