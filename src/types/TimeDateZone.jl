e4963a01b3549d845d7b78998e95c917e39a318f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-22 05:03:30 -0400

    Update TimeDateZone.jl

--- src/types/TimeDateZone.jl
+++ src/types/TimeDateZone.jl
@@ -2,30 +2,26 @@ struct TimeDateZone{T} <: AbstractTime
     timedate::TimeDate
     in_zone::T                # VariableTimeZone or FixedTimeZone
     at_zone::FixedTimeZone
-  
-    function TimeDateZone{T}(timedate::TimeDate, in_zone::T, at_zone::FixedTimeZone) where {T<:FixedTimeZone}
-        new{T}(timedate, in_zone, at_zone)
-    end
-  
-    function TimeDateZone{T}(timedate::TimeDate, in_zone::T, at_zone::FixedTimeZone) where {T<:VariableTimeZone}
-        new{T}(timedate, in_zone, at_zone)
-    end
 end
 
-TimeDateZone{T}(timedate::TimeDate, in_zone::T) where {T<:VariableTimeZone} =
+
+TimeDateZone(timedate::TimeDate, in_zone::T) where {T<:VariableTimeZone} =
     TimeDateZone{VariableTimeZone}(timedate, in_zone)
 
-TimeDateZone{T}(timedate::TimeDate, in_zone::T) where {T<:FixedTimeZone} =
+TimeDateZone(timedate::TimeDate, in_zone::T) where {T<:FixedTimeZone} =
     TimeDateZone{FixedTimeZone}(timedate, in_zone)
 
-function TimeDateZone{T}(zdt::ZonedDateTime) where {T<:VariableTimeZone}
+function TimeDateZone(zdt::ZonedDateTime) where {T}
     datetime = DateTime(zdt)
+    timedate = TimeDate(datetime)
     in_zone  = zdt.timezone
-    TimeDateZone{T}(datetime, in_zone)
+    TimeDateZone(timedate, in_zone)
 end
 
+#=
 function TimeDateZone{T}(zdt::ZonedDateTime) where {T<:FixedTimeZone}
     datetime = DateTime(zdt)
     in_zone  = zdt.timezone
     TimeDateZone{T}(datetime, in_zone)
 end
+=#
